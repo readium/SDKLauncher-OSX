d738f2f2cf49bd9b6548f2275b6f09b0836e95a3
Daniel Weck <daniel.weck@gmail.com>
2014-11-08 18:42:37 +0000

    shell script that populates a persistent epubReadingSystem.js file that contains variables which are picked-up when reader.html initialises the ReadiumSDK object (git tag, sha, human-readable version number, date/time)

---
+++ LauncherOSX/ReaderScripts/epubReadingSystem.js
@@ -0,0 +1,16 @@
+
+
+ReadiumSDK.READIUM_OSX_sha = 'a45602684e89abe0b9914088474b795e7dfb755e';
+ReadiumSDK.READIUM_OSX_tag = 'Release-0.10-96-ga456026';
+ReadiumSDK.READIUM_OSX_clean = 'false';
+
+ReadiumSDK.READIUM_SDK_sha = 'a0eba5215915f7554f8a1eea6d99a968d5007a10';
+ReadiumSDK.READIUM_SDK_tag = 'Release-0.14-63-ga0eba52';
+ReadiumSDK.READIUM_SDK_clean = 'true';
+
+ReadiumSDK.READIUM_SHARED_JS_sha = '0b53bf3f752ca4bbbc2c8f9fb690acb10e21d314';
+ReadiumSDK.READIUM_SHARED_JS_tag = 'Release-0.10-463-g0b53bf3';
+ReadiumSDK.READIUM_SHARED_JS_clean = 'true';
+
+ReadiumSDK.READIUM_dateTimeString = 'Sat  8 Nov 2014 18:40:42 GMT';
+
---
+++ LauncherOSX/ReaderScripts/epubReadingSystem.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+echo "###########################################"
+echo "###########################################"
+
+pwd=`pwd`
+echo "Path:"
+echo "${pwd}"
+echo "------"
+
+JS_FILE="${pwd}/LauncherOSX/ReaderScripts/epubReadingSystem.js"
+echo "Javascript output:"
+echo "${JS_FILE}"
+echo "------"
+
+echo $"\n" > "${JS_FILE}"
+
+GitDo() {
+ROOT_DIR=$1
+SUB_DIR=$2
+SUBMODULE=$3
+
+cd "${SUB_DIR}"
+
+echo "========================="
+echo "Git submodule:"
+echo "${SUBMODULE}"
+echo "------"
+
+GIT_DIR="${ROOT_DIR}/.git"
+echo "Git directory:"
+echo "${GIT_DIR}"
+echo "------"
+
+GIT_CWD="${SUB_DIR}"
+#test -d "${GIT_CWD}" || GIT_CWD="${SUB_DIR}"
+echo "Git submodule directory:"
+echo "${GIT_CWD}"
+echo "------"
+
+GIT_DIR_CWD=""
+# GIT_DIR_CWD="--git-dir=${GIT_DIR} --work-tree=${GIT_CWD}"
+# echo "Git path spec:"
+# echo "${GIT_DIR_CWD}"
+# echo "------"
+
+GIT_HEAD=`cat "${GIT_DIR}/HEAD"`
+echo "Git HEAD:"
+echo "${GIT_HEAD}";
+echo "------"
+
+test "${GIT_HEAD#'ref: '}" != "${GIT_HEAD}" && echo "(attached head)" && GIT_SHA=`git ${GIT_DIR_CWD} rev-parse --verify HEAD`
+test "${GIT_HEAD#'ref: '}" == "${GIT_HEAD}" && echo "(detached head)" && GIT_SHA="${GIT_HEAD}"
+
+echo "Git SHA:"
+echo "${GIT_SHA}"
+echo "------"
+
+GIT_TAG=`git ${GIT_DIR_CWD} describe --tags --long ${GIT_SHA}`
+echo "Git TAG:"
+echo "${GIT_TAG}"
+echo "------"
+
+GIT_STATUS=`git ${GIT_DIR_CWD} status --porcelain`
+echo "Git STATUS:"
+echo "${GIT_STATUS}"
+echo "------"
+
+GIT_CLEAN=false
+test -z "${GIT_STATUS}" && GIT_CLEAN=true
+echo "Git CLEAN:"
+echo "${GIT_CLEAN}"
+echo "------"
+
+echo $"\
+ReadiumSDK.READIUM_${SUBMODULE}_sha = '${GIT_SHA}';\n\
+ReadiumSDK.READIUM_${SUBMODULE}_tag = '${GIT_TAG}';\n\
+ReadiumSDK.READIUM_${SUBMODULE}_clean = '${GIT_CLEAN}';\n\
+" >> "${JS_FILE}"
+
+}
+
+GitDo "${pwd}" "${pwd}" "OSX"
+GitDo "${pwd}" "${pwd}/readium-sdk" "SDK"
+GitDo "${pwd}" "${pwd}/readium-shared-js" "SHARED_JS"
+
+READIUM_dateTimeString=`date`
+
+echo $"\
+ReadiumSDK.READIUM_dateTimeString = '${READIUM_dateTimeString}';\n\
+" >> "${JS_FILE}"
+
+cat ${JS_FILE}
+
+cd "${pwd}"
+
+echo "###########################################"
+echo "###########################################"
