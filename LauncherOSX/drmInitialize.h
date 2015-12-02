bd74e15903f1f1df76b00d3d99f565b6f6af45da
DRMinside <chyu@drminside.com>
2015-12-02 12:00:40 +0900

    Update drmInitialize.h

--- LauncherOSX/drmInitialize.h
+++ LauncherOSX/drmInitialize.h
@@ -1,9 +1,24 @@
 //
-//  LcpInitialize.h
-//  SDKLauncher-iOS
+//  DrmInitialize.h
+//  SDKLauncher-OSX
 //
-//  Created by DRMinside-IMac on 2015. 11. 5..
-//  Copyright © 2015년 The Readium Foundation. All rights reserved.
+//  Created by DRM inside Development Team on 2015-08-16.
+//  ( T.H. Kim, H.D. Yoon, H.S. Lee and C.H. Yu )
+//
+//  Copyright (c) 2015 The Readium Foundation and contributors. All rights reserved.
+//
+//  The Readium SDK is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>
 //
 #import <Foundation/Foundation.h>
 
